053f0b11aeb85f7ad7b482fd426fc92df4258921
Андрей Панасенко <xambey@yandex.ru>
2018-07-29 15:13:06 +0300

    updated queue of messages

--- Dadata/MessageQueue/MessageQueue.cs
+++ Dadata/MessageQueue/MessageQueue.cs
@@ -1,7 +1,47 @@
-﻿namespace DaData.MessageQueue
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net.Http;
+using System.Threading;
+using DaData.Singleton;
+
+namespace DaData.MessageQueue
 {
-    public sealed class MessageQueue
+    public class MessageQueue : ConcurrentQueue<HttpRequestMessage>
     {
-        private static object MLock { get; } = new object();
+        protected static object MLock { get; } = new object();
+
+        protected Timer Timer { get; }
+
+        protected HttpClient Client => HttpClientSingleton.GetInstance();
+
+        public MessageQueue()
+        {
+            Timer = new Timer(SendMessages, null, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+        }
+
+        public void Enqueue(HttpRequestMessage message)
+        {
+            lock (MLock)
+            {
+                base.Enqueue(message);    
+            }
+        }
+
+        protected virtual void SendMessages(object optional)
+        {
+            if (!IsEmpty)
+            {
+                lock (MLock)
+                {
+                    lock (Client)
+                    {
+                        while (TryDequeue(out HttpRequestMessage message))
+                        {
+                            Client.sen
+                        }
+                    }
+                }
+            }
+        }
     }
 }
